5aafe9107d5e38d5640e059281bf20cda8e911df
Kirill Fidziukevich <kfidziukevich@gmail.com>
2022-11-29 16:50:45 +0300

    Package updates

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import './i18n/i18n';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
